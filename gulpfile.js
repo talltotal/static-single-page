3a024d22786ab5f5ef6cad93542662d0589c9177
talltotal <talltotal@163.com>
2019-02-18 17:07:50 +0800

    add handlerbars

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
+const fs = require('fs')
 const gulp = require('gulp')
+const through = require('through2')
 const clean = require('gulp-clean')
 const stylelint = require('gulp-stylelint')
 const eslint = require('gulp-eslint')
@@ -9,12 +11,27 @@ const minifyCSS = require('gulp-minify-css')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const htmlmin = require('gulp-htmlmin')
+const Handlebars = require('handlebars')
+const wrap = require('gulp-wrapper')
+
+const tempData = require('./src/data.js')
 
 const Dist = 'dist'
 const Img = 'src/*.png'
-const Style = 'src/*.scss'
-const Script = 'src/*.js'
+const Style = 'src/index.scss'
+const Script = 'src/index.js'
 const Html = 'src/*.html'
+const Temp = 'src/index.hbs'
+
+const t = fs.readFileSync('src/svgIcon.hbs')
+Handlebars.registerHelper('equals', function (a, b, options) {
+  if ((a != null ? a.toString() : void 0) === (b != null ? b.toString() : void 0)) {
+    return options.fn(this)
+  } else {
+    return options.inverse(this)
+  }
+})
+Handlebars.registerPartial('svgIcon', Handlebars.compile(t.toString()))
 
 gulp.task('clean', gulp.series(function () {
   return gulp.src(Dist, {
@@ -55,6 +72,44 @@ gulp.task('script', gulp.series(function () {
     .pipe(gulp.dest(Dist))
 }))
 
+gulp.task('temp', gulp.series(function () {
+  return gulp.src(Temp)
+    .pipe(through.obj(function (file, enc, cb) {
+      if (file.isNull()) {
+        cb(null, file)
+        return
+      }
+      if (file.isBuffer()) {
+        const template = Handlebars.compile(file.contents.toString())
+        file.contents = Buffer.from(template(tempData))
+        file.path = file.path.replace(/\.hbs$/, '.html')
+      }
+      if (file.isStream()) {
+        console.log('1')
+      }
+
+      cb(null, file)
+    }))
+    .pipe(wrap({
+      header: `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="./index.css">
+        </head>`,
+      footer: `    <script type="text/javascript" src="./index.js"></script>
+      </body>
+    </html>`,
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      conservativeCollapse: true,
+    }))
+    .pipe(gulp.dest(Dist))
+}))
+
 gulp.task('html', gulp.series(function () {
   return gulp.src(Html)
     .pipe(htmlmin({
@@ -69,5 +124,5 @@ gulp.task('default', gulp.series([
   'img',
   'styles',
   'script',
-  'html'
+  'temp'
 ]))
---
+++ src/data.js
@@ -0,0 +1,89 @@
+/* data for temp */
+
+module.exports = {
+  nav: [
+    {
+      name: 'projects',
+      link: '#',
+    },
+    {
+      name: 'about as',
+      link: '#',
+    },
+    {
+      name: 'stories',
+      link: '#',
+    },
+    {
+      name: 'contact',
+      link: '#',
+    },
+  ],
+  social: [
+    {
+      link: '#',
+      icon: 'facebook',
+    },
+    {
+      link: '#',
+      icon: 'twitter',
+    },
+    {
+      link: '#',
+      icon: 'instagram',
+    },
+  ],
+  banner: [
+    {
+      bg: './bg.png',
+      icon: 'logo',
+      title: 'We Build Objects<br/>And Spaces',
+      desc: 'Support bright students today for<br/>a better tomorrow',
+    },
+    {
+      bg: './bg.png',
+      icon: 'logo',
+      title: 'We Build Objects<br/>And Spaces',
+      desc: 'Support bright students today for<br/>a better tomorrow',
+    },
+    {
+      bg: './bg.png',
+      icon: 'logo',
+      title: 'We Build Objects<br/>And Spaces',
+      desc: 'Support bright students today for<br/>a better tomorrow',
+    },
+    {
+      bg: './bg.png',
+      icon: 'logo',
+      title: 'We Build Objects<br/>And Spaces',
+      desc: 'Support bright students today for<br/>a better tomorrow',
+    },
+  ],
+  fMain: {
+    icon: '1',
+    title: 'Your Next Move',
+    desc: 'Helping talented students unlock their full<br/>potential. Help us help them thrive.',
+  },
+  feature: [
+    {
+      icon: 'a',
+      title: 'EXPERIENCE',
+      desc: '72% of the students<br/>attended on scholarship',
+    },
+    {
+      icon: 'b',
+      title: 'OUR FUTURE',
+      desc: '20% of its student body is<br/>currently under scholarship',
+    },
+    {
+      icon: 'c',
+      title: 'ACADEMIC TALENTS',
+      desc: 'More than 1000 scholarships<br/>have been awarded',
+    },
+    {
+      icon: 'd',
+      title: 'BRILLIANT MINDS',
+      desc: 'Anatolia College has always<br/>relied on donations',
+    },
+  ],
+}
